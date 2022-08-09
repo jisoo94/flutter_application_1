18c185041cdf596494b8114ab6fc948100d6be32
jisoo <max5751@naver.com>
2022-08-09 23:14:16 +0900

    0809BBANTO

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,132 @@
-// i > fm 단축키 활용
 import 'package:flutter/material.dart';
 
-// 앱의 시작점 runApp 최상위 앱 (위젯)<이 들어가야함
-// 함수는 소문자 시작
-// 클래스는 대문자 시작
-// 낙타표기법사용
-// 마지막 세미콜론 사용 ;
 void main() => runApp(MyApp());
 
-//stl 단축키
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // 앱을 총칭하는 이름
-      title: 'First app',
-      theme: ThemeData(
-          // 특정 음영 기본색 설정
-          primarySwatch: Colors.blue),
-      // 가장 먼저 보여지는 화면
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        // 앱화면에 나오는 title
-        title: Text('First app'),
+        title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello'),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/ss.gif'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'BBANTO',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'BBANTO POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(width: 10.0),
+                Text(
+                  'using lightsaber',
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(width: 10.0),
+                Text(
+                  'face hero tattoo',
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(width: 10.0),
+                Text(
+                  'fire flames',
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/ss.gif'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-// 1. flutter/material import 하기
-// 2. 컴파일러에게 시작점 알려주기 위해 void main 불러오기
-// 3. flutter 최상위 함수 runApp 함수 불러오기
-// 4. runApp함수 내에 MyApp이라는 커스텀 위젯이름을 argument로 넣어주기
-// 5. StatelessWidget으로 커스텀 위젯 생성하기
-// 6. return MaterialApp 으로 바꿔주기
-// 7. title 지정해주기
-// 8. theme: ThemeData 불러오기
-// 9. primarySwatch 불러오기
-// 10. blue 색상으로 지정해주기
-// 11. home: 에서 MyHomePage() 지정해주기
