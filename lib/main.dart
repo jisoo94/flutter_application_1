d15c88064fd0fd3f920e4ab3ac72363cfe661c1c
jisoo <max5751@naver.com>
2022-08-23 20:25:05 +0900

    0823DiceGame

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,157 @@
 import 'package:flutter/material.dart';
+import 'dice.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyButtons(),
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
     );
   }
 }
 
-class MyButtons extends StatelessWidget {
-  const MyButtons({Key? key}) : super(key: key);
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('buttons'),
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
         centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                // print('textbutton');
-              },
-              onLongPress: () {
-                print('textbutton');
-              },
-              child: Text(
-                'text button',
-                style: TextStyle(fontSize: 20.0),
+      body: Builder(
+        builder: (context) {
+          return GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  Center(
+                    child: Image(
+                      image: AssetImage('image/chef.gif'),
+                      width: 170.0,
+                      height: 190.0,
+                    ),
+                  ),
+                  Form(
+                      child: Theme(
+                          data: ThemeData(
+                              primaryColor: Colors.teal,
+                              inputDecorationTheme: InputDecorationTheme(
+                                  labelStyle: TextStyle(
+                                      color: Colors.teal, fontSize: 15.0))),
+                          child: Container(
+                            padding: EdgeInsets.all(40.0),
+                            child: Column(
+                              children: <Widget>[
+                                TextField(
+                                  controller: controller,
+                                  decoration: InputDecoration(
+                                      labelText: 'Enter "image"'),
+                                  keyboardType: TextInputType.emailAddress,
+                                ),
+                                TextField(
+                                  controller: controller2,
+                                  decoration: InputDecoration(
+                                      labelText: 'Enter Password'),
+                                  keyboardType: TextInputType.text,
+                                  obscureText: true,
+                                ),
+                                SizedBox(
+                                  height: 40.0,
+                                ),
+                                ButtonTheme(
+                                    minWidth: 100.0,
+                                    height: 50.0,
+                                    child: RaisedButton(
+                                        color: Colors.orangeAccent,
+                                        child: Icon(
+                                          Icons.arrow_forward,
+                                          color: Colors.white,
+                                          size: 35.0,
+                                        ),
+                                        onPressed: () {
+                                          if (controller.text == 'image' &&
+                                              controller2.text == '1234') {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (BuildContext
+                                                            context) =>
+                                                        Dice()));
+                                          } else if (controller.text ==
+                                                  'image' &&
+                                              controller2.text != '1234') {
+                                            showSnackBar2(context);
+                                          } else if (controller.text !=
+                                                  'image' &&
+                                              controller2.text == '1234') {
+                                            showSnackBar3(context);
+                                          } else {
+                                            showSnackBar(context);
+                                          }
+                                        })),
+                              ],
+                            ),
+                          )))
+                ],
               ),
-              style: TextButton.styleFrom(primary: Colors.red),
             ),
-            ElevatedButton(
-              onPressed: () {
-                print('elevatedbutton');
-              },
-              child: Text('Elevated button'),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.orangeAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  elevation: 0.0),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print('Outlined button');
-              },
-              child: Text('Outlined button'),
-              style: OutlinedButton.styleFrom(
-                primary: Colors.green,
-                // side: BorderSide(
-                //   color: Colors.black87,
-                //   width: 2.0
-                // ),
-              ),
-            ),
-            TextButton.icon(
-              onPressed: () {
-                print('Icon button');
-              },
-              icon: Icon(
-                Icons.home,
-                size: 30.0,
-                //color: Colors.black87,
-              ),
-              label: Text('Go to home'),
-              style: TextButton.styleFrom(primary: Colors.purple),
-            ),
-            ElevatedButton.icon(
-              onPressed: () {
-                print('Go to Home');
-              },
-              icon: Icon(
-                Icons.home,
-                size: 20,
-              ),
-              label: Text('Go to Home'),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.black, minimumSize: Size(200, 50)),
-            ),
-            TextButton.icon(
-              onPressed: null,
-              icon: Icon(
-                Icons.home,
-                size: 30.0,
-                //color: Colors.black87,
-              ),
-              label: Text('Go to home'),
-              style: TextButton.styleFrom(
-                  primary: Colors.purple, onSurface: Colors.pink),
-            ),
-            // 버튼을 한쪽으로 정렬
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              buttonPadding: EdgeInsets.all(20.0),
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text('TextButton'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('ElevatedButton'),
-                ),
-              ],
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '로그인 정보를 다시 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '비밀번호가 일치하지 않습니다',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      'dice의 철자를 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
